af7ebf4d51b7001041fb9036558bdb981451ccd7
welina82 <shakotan0822@gmail.com>
2025-05-28 10:32:28 +0900

    kadai_016をアップロード

---
+++ kadai_016/class.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>kadai_016</title>
+</head>
+<body>
+<p>
+  <?php
+  class Food {
+      private $name;
+      private $price;
+
+      public function __construct(string $name, int $price) {
+          $this->name = $name;
+          $this->price = $price;
+      }
+
+      public function show_price() {
+          echo $this->price . '<br>';
+      }
+  }
+
+  class Animal {
+      private $name;
+      private $height;
+      private $weight;
+
+      public function __construct(string $name, int $height, int $weight) {
+          $this->name = $name;
+          $this->height = $height;
+          $this->weight = $weight;
+      }
+
+      public function show_height() {
+          echo $this->height . '<br>';
+      }
+  }
+
+  // インスタンス化する
+  $food = new Food('potato', 250);
+  $animal = new Animal('dog', 60, 5000);
+
+  print_r($food);
+  echo '<br>';
+  print_r($animal);
+  echo '<br>';
+
+  // メソッドへアクセスする
+  $food->show_price();
+  $animal->show_height();
+  ?>
+</p>
+</body>
+</html>
